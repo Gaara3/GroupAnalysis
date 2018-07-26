54a96f96a01a1fc99809de8e77df1dfd9832c004
Shixiong Liu <liushx3@outlook.com>
2018-07-26 17:35:01 +0800

    init

--- GroupAnalysis/MiningTool.cpp
+++ GroupAnalysis/MiningTool.cpp
@@ -12,12 +12,16 @@ MiningTool::~MiningTool()
 }
 
 double MiningTool::miningDistance(OriginPoint a, OriginPoint b) {
+	//同一目标不同点迹，不计算距离，聚类时直接过滤
+	/*if (a.getTargetID() == b.getTargetID())
+		return -1;*/		//先一起聚类，在簇内再去重
+
 	int tDiff = abs(a.getPosixtime() - b.getPosixtime());
 	double distance = distanceBetweenPoints(a.getLongitude(), a.getLatitude(), b.getLongitude(), b.getLatitude());
 	double angleDiff = abs(a.getAngle() - b.getAngle());
 	double speedDiff = abs(a.getSpeed() - b.getSpeed());
-	int sameTarget = a.getTargetID() == b.getTargetID() ? 0 : 1;
 
+	return 
 }
 
 
